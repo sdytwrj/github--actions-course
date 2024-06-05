4a9d3aa6ef6122648dcf393851b48cc2398929fc
sdytwrj <168611983+sdytwrj@users.noreply.github.com>
2024-06-05 16:06:15 -0400

    Update main.py

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -22,7 +22,7 @@ def ping_url(url, delay, max_trials):
 def run():
     website_url = os.getenv('INPUT_URL')
     delay = int(os.getenv('INPUT_DELAY'))
-    max_trials = in(os.getenv('INPUT_MAX_TRIALS'))
+    max_trials = int(os.getenv('INPUT_MAX_TRIALS'))
 
     website_reachable = ping_url(website_url, delay, max_trials)
     
